09b02e771d622a8a95188508266678dc696ee7b3
Muhammad Suleman <m.suleman275@gmail.com>
2023-12-17 16:15:01 +0300

    set up new scene for base of game and set up hand tracking

--- Assets/Scripts/HandTrackerScript.cs
+++ Assets/Scripts/HandTrackerScript.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 using NuitrackSDK;
 
 public class HandTrackerScript : MonoBehaviour {
-    
+    [SerializeField] private bool visulaiseHands;
+    [SerializeField] private GameObject handPrefab;
+
+    private GameObject leftHandGO;
+    private GameObject rightHandGO;
+
     UserData.SkeletonData.Joint leftHandJoint;
     UserData.SkeletonData.Joint rightHandJoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (visulaiseHands) {
+            leftHandGO = Instantiate(handPrefab);
+            rightHandGO = Instantiate(handPrefab);
+
+            leftHandGO.transform.SetParent(transform);
+            rightHandGO.transform.SetParent(transform);
+        }
     }
 
     // Update is called once per frame
@@ -21,11 +32,17 @@ public class HandTrackerScript : MonoBehaviour {
             rightHandJoint = NuitrackManager.Users.Current.Skeleton.GetJoint(nuitrack.JointType.RightHand);
 
             // Log the positions of left and right hands
-            Debug.Log("Left Hand Position: " + leftHandJoint.Position);
-            Debug.Log("Right Hand Position: " + rightHandJoint.Position);
+            //Debug.Log("Left Hand Position: " + leftHandJoint.Position);
+            //Debug.Log("Right Hand Position: " + rightHandJoint.Position);
+
+            //Upate the hand game object positions on x and y axis
+            if (visulaiseHands) {
+                leftHandGO.transform.localPosition = new Vector3(leftHandJoint.Position.x, leftHandJoint.Position.y, 0);
+                rightHandGO.transform.localPosition = new Vector3(rightHandJoint.Position.x, rightHandJoint.Position.y, 0);
+            }
         }
         else {
-            Debug.Log("Hands not in View");
+            Debug.Log("Body not in View");
         }
     }
 }
